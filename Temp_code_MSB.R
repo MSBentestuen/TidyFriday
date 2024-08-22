0fff86e998ea3436f84c1765c5ef892236982995
Marlene Schouby Bentestuen <MARBOC@onerm.dk>
2024-08-22 14:53:54 +0200

    Bee GIF added

---
+++ Temp_code_MSB.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+
+colony <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-01-11/colony.csv')
+stressor <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-01-11/stressor.csv')
+
+# In the following we will focus on total number of colonies per state during 2015-2021. 
+
+colony_balance <- colony %>% 
+  group_by(year, state) %>% 
+  summarise(year_sum = sum(colony_added - colony_lost, na.rm = TRUE))
+
+colony_balance %>% 
+  filter(state != "United States") %>% 
+  ggplot(aes(year, year_sum)) + 
+  geom_line() + 
+  facet_wrap(~ state) + 
+  theme_minimal()
+
